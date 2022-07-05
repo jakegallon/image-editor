cdd0187309a84becc2d4f7c93da36478b9b51467
jakegallon <jakegallon83@gmail.com>
2022-07-05 22:04:16 +0100

    Include and Implement FlatLaf
    https://github.com/JFormDesigner/FlatLaf

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+
         Window window = new frame.Window();
         window.setVisible(true);
     }
