112d7d45f51b07669df4bfdd634fb4dd3ce58b61
jakegallon <jakegallon83@gmail.com>
2022-07-22 23:10:36 +0100

    Rename Window to Frame and create placeholder panels for each component in Frame

--- src/frame/Frame.java
+++ src/frame/Frame.java
@@ -5,16 +5,17 @@ import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
-public class Window extends JFrame {
+public class Frame extends JFrame {
 
     JSplitPane leftMiddle, leftMiddleRight;
     JPanel leftPanel, rightPanel, topPanel;
+
     CanvasPanel canvasPanel;
     InfoPanel infoPanel;
 
-    int leftPanelWidth = 300, rightPanelWidth = 300;
+    int leftPanelWidth = 350, rightPanelWidth = 350;
 
-    public Window() {
+    public Frame() {
         setVisible(true);
         initializeWindow();
         createLayout();
@@ -30,18 +31,56 @@ public class Window extends JFrame {
     private void createLayout(){
         Container pane = getContentPane();
 
-        // Main panels
-        leftPanel=new JPanel();
+        // Temporary panel creations
+        // leftPanel - toolBar
+        JPanel toolBar = new JPanel();
+        toolBar.setBackground(Color.gray);
+        toolBar.setPreferredSize(new Dimension(40, 0));
+        // leftPanel - brushSettings
+        JPanel brushSettings = new JPanel();
+        brushSettings.setBackground(Color.cyan);
+        brushSettings.setMinimumSize(new Dimension(0, 250));
+        // leftPanel - colorSettings
+        JPanel colorSettings = new JPanel();
+        colorSettings.setBackground(Color.green);
+        colorSettings.setMinimumSize(new Dimension(0, 250));
+        // rightPanel - canvasOverview
+        JPanel canvasOverview = new JPanel();
+        canvasOverview.setBackground(Color.RED);
+        canvasOverview.setMinimumSize(new Dimension(0, 250));
+        // rightPanel - animationPanel
+        JPanel animationPanel = new JPanel();
+        animationPanel.setBackground(Color.ORANGE);
+        animationPanel.setMinimumSize(new Dimension(0, 250));
+        // rightPanel - layerPanel
+        JPanel layerPanel = new JPanel();
+        layerPanel.setBackground(Color.YELLOW);
+        layerPanel.setMinimumSize(new Dimension(0, 250));
+
+        // left panel
+        leftPanel=new JPanel(new BorderLayout());
         leftPanel.setBackground(Color.RED);
         leftPanel.setMinimumSize(new Dimension(250, 0));
-        rightPanel=new JPanel();
-        rightPanel.setBackground(Color.BLUE);
+
+        leftPanel.add(toolBar, BorderLayout.LINE_START);
+        JSplitPane brushColor = new JSplitPane(JSplitPane.VERTICAL_SPLIT, brushSettings, colorSettings);
+        brushColor.setResizeWeight(1.0);
+        leftPanel.add(brushColor);
+
+        // Right panel
+        rightPanel=new JPanel(new BorderLayout());
+        JSplitPane canvasAnimation = new JSplitPane(JSplitPane.VERTICAL_SPLIT, canvasOverview, animationPanel);
+        JSplitPane canvasAnimationLayer = new JSplitPane(JSplitPane.VERTICAL_SPLIT, canvasAnimation, layerPanel);
+        canvasAnimationLayer.setResizeWeight(1.0);
+
+        rightPanel.add(canvasAnimationLayer);
         rightPanel.setMinimumSize(new Dimension(250, 0));
+
+        // Main panel
         JPanel middlePanel=new JPanel();
         middlePanel.setMinimumSize(new Dimension(250, 0));
         middlePanel.setLayout(new BorderLayout());
 
-        // Middle sub panels
         topPanel=new JPanel();
         topPanel.setBackground(Color.MAGENTA);
         topPanel.setPreferredSize(new Dimension(pane.getWidth(), 30));
