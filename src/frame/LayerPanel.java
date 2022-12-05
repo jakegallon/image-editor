3c984309d50b8e098127224803d39232c1b3574e
jakegallon <jakegallon83@gmail.com>
2022-12-05 00:05:58 +0000

    Add ability to click and drag LayerWidgets in LayerPanel to reorder in the UI

--- src/frame/LayerPanel.java
+++ src/frame/LayerPanel.java
@@ -1,11 +1,17 @@
 package frame;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class LayerPanel extends JPanel {
 
@@ -18,10 +24,20 @@ public class LayerPanel extends JPanel {
 
     LayerWidget activeLayerWidget;
 
+    private static BufferedImage gradient;
+    private static BufferedImage gradient_alt;
+
     public LayerPanel() {
         setMinimumSize(new Dimension(0, 250));
         setPreferredSize(new Dimension(0, 400));
         setLayout(new BorderLayout());
+
+        try {
+            gradient = ImageIO.read(Objects.requireNonNull(getClass().getResource("/res/gradient.png")));
+            gradient_alt = ImageIO.read(Objects.requireNonNull(getClass().getResource("/res/gradient_alt.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         layerPanelHandler = new LayerPanelHandler(this);
         add(layerPanelHandler, BorderLayout.PAGE_START);
@@ -117,76 +133,149 @@ public class LayerPanel extends JPanel {
         activeLayerWidget = layerWidget;
     }
 
+    private int getIndexOfLayerWidget(LayerWidget layerWidget) {
+        return layerWidgets.indexOf(layerWidget);
+    }
+
+    private boolean isValidLayerWidgetsIndex(int index) {
+        return index >= 0 && index < layerWidgets.size();
+    }
+
+    private void swapLayerWidgetToIndex(LayerWidget layerWidget, int index) {
+        int oldIndex = getIndexOfLayerWidget(layerWidget);
+        Collections.swap(layerWidgets, oldIndex, index);
+        redrawLayerContainerFromLayerWidgets();
+    }
+
+    private void redrawLayerContainerFromLayerWidgets() {
+        layerContainer.removeAll();
+
+        for(int i = layerWidgets.size() - 1; i >= 0; i--) {
+            LayerWidget layerWidget = layerWidgets.get(i);
+            layerContainer.add(layerWidget, 0);
+        }
+
+        layerContainer.revalidate();
+        layerContainer.repaint();
+    }
+
     private static final Dimension SLIDER_THUMB_SIZE = (Dimension) UIManager.get("Slider.thumbSize");
     private static final int LAYER_WIDGET_HEIGHT = 42;
 
-    private class LayerWidget extends JPanel implements MouseListener {
+    private class LayerWidget extends JPanel implements MouseListener, MouseMotionListener {
 
         Layer layer;
+        private SpringLayout springLayout;
+
+        private JLabel layerName;
+        private JPanel layerImage;
+        private JSlider layerOpacity;
+        private JButton deleteButton;
+        private JButton lockButton;
+        private JButton renameButton;
+        private JToggleButton visibleButton;
 
         private LayerWidget(Layer layer) {
             this.layer = layer;
 
+            init();
+        }
+
+        private void init() {
             addMouseListener(this);
+            addMouseMotionListener(this);
 
             setPreferredSize(new Dimension(layerContainer.getWidth(), LAYER_WIDGET_HEIGHT));
             setMaximumSize(new Dimension(layerContainer.getWidth(), LAYER_WIDGET_HEIGHT));
             setMinimumSize(new Dimension(layerContainer.getWidth(), LAYER_WIDGET_HEIGHT));
 
-            SpringLayout springLayout = new SpringLayout();
+            springLayout = new SpringLayout();
             setLayout(springLayout);
 
-            JLabel layerName = new JLabel(layer.getName());
+            addWidgetComponents();
+            alignBase();
+
+            if(Controller.isActiveLayer(layer)){
+                setNewActiveLayerWidget(this);
+            }
+        }
+
+        private void addWidgetComponents() {
+            layerName = new JLabel(layer.getName());
             add(layerName);
 
-            JPanel layerImage = new JPanel() {
-              @Override
-              protected void paintComponent(Graphics g) {
-                  super.paintComponent(g);
-                  g.drawImage(layer.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
-                  if(Controller.isActiveLayer(layer)){
-                      repaint();
-                  }
-              }
+            layerImage = new JPanel() {
+                @Override
+                protected void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(layer.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+                    if(Controller.isActiveLayer(layer)){
+                        repaint();
+                    }
+                }
             };
             layerImage.setBorder(new LineBorder(Color.black));
             layerImage.setPreferredSize(new Dimension(LAYER_WIDGET_HEIGHT - 10, LAYER_WIDGET_HEIGHT - 10));
             add(layerImage);
 
-            JSlider layerOpacity = new JSlider(0, 255, 255);
+            layerOpacity = new JSlider(0, 255, 255);
             add(layerOpacity);
 
             layerOpacity.addChangeListener(e -> layer.setOpacity(layerOpacity.getValue()));
 
-            JButton deleteButton = new JButton("X");
+            deleteButton = new JButton("X");
             deleteButton.setBackground(Color.RED);
             deleteButton.setPreferredSize(new Dimension(16, 16));
             add(deleteButton);
 
-            deleteButton.addActionListener(e -> {
-                ArrayList<Layer> l = Controller.getActiveCanvas().layers;
-                deleteLayer(l.indexOf(layer));
-                l.remove(layer);
-            });
+            deleteButton.addActionListener(deleteListener());
 
-            JButton lockButton = new JButton("L");
+            lockButton = new JButton("L");
             lockButton.setBackground(Color.YELLOW);
             lockButton.setPreferredSize(new Dimension(16, 16));
             add(lockButton);
 
-            JButton renameButton = new JButton("R");
+            lockButton.addActionListener(lockListener());
+
+            renameButton = new JButton("R");
             renameButton.setBackground(Color.GREEN);
             renameButton.setPreferredSize(new Dimension(16, 16));
             add(renameButton);
 
-            JToggleButton visibleButton = new JToggleButton("V");
+            renameButton.addActionListener(renameListener());
+
+            visibleButton = new JToggleButton("V");
             visibleButton.setBackground(Color.GRAY);
             visibleButton.setPreferredSize(new Dimension(16, 16));
             add(visibleButton);
             visibleButton.setSelected(true);
 
-            visibleButton.addChangeListener(e -> layer.setVisible(visibleButton.isSelected()));
+            visibleButton.addChangeListener(visibleListener());
+        }
 
+        private ActionListener deleteListener() {
+            return e -> {
+                ArrayList<Layer> l = Controller.getActiveCanvas().layers;
+                deleteLayer(l.indexOf(layer));
+                l.remove(layer);
+            };
+        }
+
+        //todo
+        private ActionListener lockListener() {
+            return null;
+        }
+
+        //todo
+        private ActionListener renameListener() {
+            return null;
+        }
+
+        private ChangeListener visibleListener() {
+            return e -> layer.setVisible(visibleButton.isSelected());
+        }
+
+        private void alignBase() {
             springLayout.putConstraint(SpringLayout.NORTH, layerImage, 5, SpringLayout.NORTH, this);
             springLayout.putConstraint(SpringLayout.WEST, layerImage, 5, SpringLayout.WEST, this);
             springLayout.putConstraint(SpringLayout.NORTH, visibleButton, 0, SpringLayout.NORTH, layerImage);
@@ -202,10 +291,50 @@ public class LayerPanel extends JPanel {
             springLayout.putConstraint(SpringLayout.EAST, lockButton, 0, SpringLayout.EAST, deleteButton);
             springLayout.putConstraint(SpringLayout.NORTH, renameButton, 0, SpringLayout.NORTH, deleteButton);
             springLayout.putConstraint(SpringLayout.EAST, renameButton, -4, SpringLayout.WEST, deleteButton);
+        }
 
-            if(Controller.isActiveLayer(layer)){
-                setNewActiveLayerWidget(this);
+        JPanel floatGradient;
+
+        private void floatWidget() {
+            alignFloated();
+
+            floatGradient = new JPanel() {
+                @Override
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    if(activeLayerWidget == this.getParent()) {
+                        g.drawImage(gradient, 0, 0, floatGradient.getWidth(), floatGradient.getHeight(), null);
+                    } else {
+                        g.drawImage(gradient_alt, 0, 0, floatGradient.getWidth(), floatGradient.getHeight(), null);
+                    }
+                }
+            };
+            add(floatGradient);
+            springLayout.putConstraint(SpringLayout.WEST, floatGradient, 0, SpringLayout.WEST, this);
+            springLayout.putConstraint(SpringLayout.NORTH, floatGradient, 0, SpringLayout.NORTH, this);
+            springLayout.putConstraint(SpringLayout.EAST, floatGradient, -5, SpringLayout.WEST, layerImage);
+            springLayout.putConstraint(SpringLayout.SOUTH, floatGradient, 0, SpringLayout.SOUTH, this);
+
+            revalidate();
+        }
+
+        private void unfloatWidget() {
+            alignNormal();
+
+            if(floatGradient != null) {
+                remove(floatGradient);
             }
+
+            revalidate();
+            repaint();
+        }
+
+        private void alignNormal() {
+            springLayout.putConstraint(SpringLayout.WEST, layerImage, 5, SpringLayout.WEST, this);
+        }
+
+        private void alignFloated() {
+            springLayout.putConstraint(SpringLayout.WEST, layerImage, 25, SpringLayout.WEST, this);
         }
 
         public void setActive() {
@@ -226,30 +355,73 @@ public class LayerPanel extends JPanel {
             setPreferredSize(new Dimension(width, LAYER_WIDGET_HEIGHT));
         }
 
+        private boolean isMouseOverWidget = false;
+
+        private boolean isMousePressed = false;
+
+        private int initialIndex;
+        private int index;
+        private int currentIndexOffset;
+        private int trueIndex;
+
         @Override
         public void mouseClicked(MouseEvent e) {
-            Controller.getActiveCanvas().setActiveLayer(layer);
-            setNewActiveLayerWidget(this);
+
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-
+            isMousePressed = true;
+            currentIndexOffset = 0;
+            index = getIndexOfLayerWidget(this);
+            initialIndex = index;
+            trueIndex = index;
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
+            if(isMouseOverWidget && index == initialIndex) {
+                setLayerAsActive();
+            }
 
+            unfloatWidget();
+            isMousePressed = false;
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-
+            isMouseOverWidget = true;
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
+            if(!getVisibleRect().contains(e.getPoint())) {
+                isMouseOverWidget = false;
+                if (isMousePressed) floatWidget();
+            }
+        }
 
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            int relativeIndexTarget = Math.floorDiv(e.getPoint().y, LAYER_WIDGET_HEIGHT);
+            if(relativeIndexTarget != currentIndexOffset) {
+                currentIndexOffset = relativeIndexTarget;
+                if(isValidLayerWidgetsIndex(index + currentIndexOffset)){
+                    trueIndex = index + currentIndexOffset;
+                    swapLayerWidgetToIndex(this, trueIndex);
+                    index = getIndexOfLayerWidget(this);
+                }
+            }
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
+        }
+
+        private void setLayerAsActive() {
+            Controller.getActiveCanvas().setActiveLayer(layer);
+            setNewActiveLayerWidget(this);
         }
     }
 }
