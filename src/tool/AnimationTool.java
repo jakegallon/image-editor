c7e58ad3570dab9c409e974402ae2259a4e998a6
jakegallon <jakegallon83@gmail.com>
2022-12-06 11:47:34 +0000

    Add usage guide to AnimationTool as replacement for properties

--- src/tool/AnimationTool.java
+++ src/tool/AnimationTool.java
@@ -14,7 +14,15 @@ public class AnimationTool extends BaseTool {
 
     @Override
     public void attachProperties(JPanel panel) {
+        JLabel title = new JLabel("Animation Tool");
+        title.setFont(title.getFont().deriveFont(16f));
+        panel.add(title);
 
+        JLabel description = new JLabel("<html>This tool is used alongside the grid to create the<br>illusion of animation where each grid cell is a<br>frame in the animation.</html>");
+        panel.add(description);
+
+        JLabel usage = new JLabel("<html><br>To use this tool, click inside the grid's cells in the<br>desired order to make an animation appear in the<br>animation tab on the right.</html>");
+        panel.add(usage);
     }
 
     @Override
